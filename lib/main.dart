12557f4c5596ab5e927980c5b67c0d2d2e8dded4
Anas1Ashkar <anas_ashker_2000_ps@yahoo.com>
2023-08-04 17:57:12 +0300

    ass1

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/screens/quiz_app.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      title: 'Quiz app',
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
